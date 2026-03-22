907baf980bb007c012d5e3056969a7c8c7ae1920
lxchurbakov <lxchurbakov@gmail.com>
2026-03-22 14:27:42 +0300

    threads on server side

--- server/index.ts
+++ server/index.ts
@@ -4,11 +4,13 @@ config();
 
 import WebsocketServer from './plugins/websocket-server';
 import Entrypoint from './plugins/entrypoint';
+import Threads from './plugins/threads';
 // import Boards from './plugins/boards';
 // import Workers from './plugins/cloud-workers';
 
 const entrypoint = new Entrypoint();
 const wss = new WebsocketServer(entrypoint);
+const threads = new Threads(wss);
 
 // const boards = new Boards(entrypoint);
 // const workers = new Workers(entrypoint, wss);
---
+++ server/plugins/threads.ts
@@ -0,0 +1,73 @@
+import WebsocketServer from "./websocket-server";
+
+const threads = (() => {
+    const subscribers = new Map<string, string[]>();
+
+    const add = (threadId: string, clientId: string) => {
+        const subs = subscribers.get(threadId) ?? [];
+
+        return subscribers.set(threadId, [...subs, clientId]);
+    };
+
+    const remove = (threadId: string, clientId: string) => {
+        const subs = subscribers.get(threadId) ?? [];
+
+        return subscribers.set(threadId, subs.filter(($) => $ !== clientId));
+    };
+
+    const dismantle = (clientId: string) => {
+        for (let threadId in subscribers) {
+            remove(threadId, clientId);
+        }
+    };
+
+    const list = (threadId: string) => {
+        return subscribers.get(threadId) ?? [];
+    };
+
+    return { add, remove, dismantle, list };
+})();
+
+export default class {
+    constructor (private wss: WebsocketServer) {
+        //
+        // Subscribing and unsubscribing from thread
+        //
+        this.wss.onMessage.on(({ clientId, data }) => {
+            if (data?.type === 'thread/subscribe') {
+                const { name } = data;
+
+                threads.add(name, clientId);
+            }
+
+            if (data?.type === 'thread/unsubscribe') {
+                const { name } = data;
+
+                threads.remove(name, clientId);
+            }
+        });
+
+        this.wss.onDisconnect.on(({ clientId }) => {
+            threads.dismantle(clientId);
+        });
+
+        // Dispatching messages in thread
+        // (basically resending them)
+        this.wss.onMessage.on(({ clientId, data }) => {
+            console.log('debug message', data)
+            if (data?.type === 'thread/message') {
+                const { message, threadId } = data;
+
+                console.log('thread/message', { message, threadId });
+                
+                for (let recepientClientId of threads.list(threadId)) {
+                    try {
+                        this.wss.send(recepientClientId, { type: 'thread/message', threadId, clientId, message });
+                    } catch (e) {
+                        //
+                    }
+                }
+            }
+        });
+    }
+};
